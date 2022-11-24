617c0a86d5f925c5f2e43b0473373044e841fa78
Kahoru Hagiya <kahoruda@yahoo.co.jp>
2022-11-24 10:47:37 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "instance_type" {
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default     = "example-01"
+  default     = "kahoru-example"
 }
 
 variable "owned_by" {
